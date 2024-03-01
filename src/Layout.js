f0c41f34a506e6a78d1ac2348abd8ec17d9d2213
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-03-01 21:51:49 +0300

    change

--- src/Layout.js
+++ src/Layout.js
@@ -4,8 +4,8 @@ import { useSelector } from "react-redux";
 const Layout = () => {
   const cartItem = useSelector((state) => state.cart.cart);
   return (
-    <div>
-      <nav className="flex gap-4 justify-center items-center mt-4">
+    <div className="fixed flex items-center justify-center  z-20 top-[-16px] h-16 w-full bg-gray-600 ">
+      <nav className=" text-white   w-full flex gap-4  justify-center items-center mt-4">
         <Link to="/">Home</Link>
         <Link to="/checkout" className="flex">
           Basket <p className="font-bold ml-[3px]">({cartItem.length})</p>
