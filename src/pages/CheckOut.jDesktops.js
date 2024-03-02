b3ddf0dddbb49f6ac0dc5fc580cc31c08cd78a31
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-03-03 00:26:42 +0300

    sdfs

--- src/pages/CheckOut.jDesktops.js
+++ src/pages/CheckOut.jDesktops.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import styles from "../features/style.module.css";
-import { decrement, increment, deleteFunc } from "../features/cardSlice";
+import {
+  decrement,
+  increment,
+  deleteFunc,
+  addtoCart,
+} from "../features/cardSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -40,111 +45,120 @@ const CheckOutDesktop = () => {
 
   return (
     <>
-      <div className="text-center  text-2xl font-bold mt-4 flex justify-around">
-        <div>Your general orders count: {cartItems.length}</div>
-        <div>Total Price: {totalPrice}AZN</div>
-      </div>
-      <div className="w-1/2 flex flex-col m-auto max-lg:w-[85%] max-md:px-2 py-4">
-        <div className="w-full my-4">
-          <ul className=" w-full flex  rounded ">
-            <div className="w-1/2 text-left ">Image</div>
-            <div className="w-1/2 text-left pl-3 ">Product</div>
-            <div className="w-1/2 text-left ">Price</div>
-            <div className="w-1/2 text-left ">Total</div>
-            <div className="w-1/2 text-left ">QTY</div>
-            <div className="w-1/2 text-center ">Action</div>
-          </ul>
-        </div>
-        {cartItems.map((item) => (
-          <div className="flex  justify-between items-center  ">
-            <div
-              key={item.id}
-              className="border border-600 h-1/2  flex justify-between rounded hover:bg-gray-200 items-center  mb-4"
-            >
-              <div className="w-1/2 h-full">
-                <img src={item.thumbnail} className=" h-full" alt="sd" />
-              </div>
-              <div className="w-1/2   pl-3">
-                <h1 className="text-[14px]">
-                  <strong>{item.title.split("").slice(0, 13)}...</strong>
-                </h1>
-                <p className="text-[12px] mt-2">quantity: {item.quantity}</p>
-              </div>
-              <div className="w-1/2">{item.price}</div>
-              <div className="w-1/2 ">
-                <div>{item.price * item.count}</div>
-              </div>
-              <div className="w-1/2 flex items-center">
-                <button
-                  onClick={() => decrementBtn(item)}
-                  className="max-md:w-full px-[9.2px] py-2 "
+      {cartItems.length > 0 ? (
+        <div>
+          <div className="text-center text-2xl font-bold flex justify-around">
+            <div>Your general orders count: {cartItems.length}</div>
+            <div>Total Price: {totalPrice}AZN</div>
+          </div>
+          <div className="w-1/2 flex flex-col m-auto max-lg:w-[85%] max-md:px-2 py-4">
+            <div className="w-full my-4">
+              <ul className=" w-full flex rounded ">
+                <div className="w-1/2 text-left ">Image</div>
+                <div className="w-1/2 text-left pl-3 ">Product</div>
+                <div className="w-1/2 text-left ">Price</div>
+                <div className="w-1/2 text-left ">Total</div>
+                <div className="w-1/2 text-left ">QTY</div>
+                <div className="w-1/2 text-center ">Action</div>
+              </ul>
+            </div>
+            {cartItems.map((item) => (
+              <div className="flex justify-between items-center  ">
+                <div
+                  key={item.id}
+                  className="border border-600 h-1/2  flex justify-between rounded hover:bg-gray-200 items-center  mb-4"
                 >
-                  -
-                </button>
-                <button className="max-md:w-full px-[9.2px] py-2">
-                  {item.count}
-                </button>
-                <button
-                  onClick={() => incrementBtn(item)}
-                  className="max-md:w-full px-[9.2px] py-2"
-                >
-                  +
-                </button>
+                  <div className="w-1/2 h-full">
+                    <img src={item.thumbnail} className="h-full" alt="sd" />
+                  </div>
+                  <div className="w-1/2 pl-3">
+                    <h1 className="text-[14px]">
+                      <strong>{item.title.split("").slice(0, 13)}...</strong>
+                    </h1>
+                    <p className="text-[12px] mt-2">
+                      quantity: {item.quantity}
+                    </p>
+                  </div>
+                  <div className="w-1/2">{item.price}</div>
+                  <div className="w-1/2 ">
+                    <div>{item.price * item.count}</div>
+                  </div>
+                  <div className="w-1/2 flex items-center">
+                    <button
+                      onClick={() => decrementBtn(item)}
+                      className="max-md:w-full px-[9.2px] py-2"
+                    >
+                      -
+                    </button>
+                    <button className="max-md:w-full px-[9.2px] py-2">
+                      {item.count}
+                    </button>
+                    <button
+                      onClick={() => incrementBtn(item)}
+                      className="max-md:w-full px-[9.2px] py-2"
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="w-[145px] flex justify-center ">
+                  <div
+                    className="rounded-full bg-gray-200 hover:bg-gray-400 cursor-pointer mt-[-10px] p-2 h-[30px] w-[30px] flex justify-center items-center"
+                    onClick={() => deleteBtn(item)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className="h-full w-full" />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="w-[145px]  flex justify-center ">
-              <div
-                className=" rounded-full bg-gray-200 hover:bg-gray-400 cursor-pointer mt-[-10px]  p-2 h-[30px] w-[30px] flex justify-center items-center"
-                onClick={() => deleteBtn(item)}
-              >
-                <FontAwesomeIcon icon={faTrash} className="h-full w-full" />
+            ))}
+            <div className="flex max-md:flex-col">
+              <div className="w-[50%] border border-1 p-2 border-r-0">
+                <h1>Choose shipping mode:</h1>
+                <div>
+                  <input
+                    type="radio"
+                    id="free"
+                    name="shippingMode"
+                    value="free"
+                    checked={shippingMode === "free"}
+                    onChange={() => setShippingMode("free")}
+                  />
+                  <label htmlFor="free"> Free</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    id="delivery"
+                    name="shippingMode"
+                    value="delivery"
+                    checked={shippingMode === "delivery"}
+                    onChange={() => setShippingMode("delivery")}
+                  />
+                  <label htmlFor="delivery"> Delivery: 9AZN</label>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-        {/* <div>Common Price: {reducer}</div> */}
-        <div className="flex max-md:flex-col">
-          <div className="w-[50%] border border-1 p-2 border-r-0">
-            <h1>Choose shoppig mode:</h1>
-            <div>
-              <input
-                type="radio"
-                id="free"
-                name="shippingMode"
-                value="free"
-                checked={shippingMode === "free"}
-                onChange={() => setShippingMode("free")}
-              />
-              <label htmlFor="free"> Free</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="delivery"
-                name="shippingMode"
-                value="delivery"
-                checked={shippingMode === "delivery"}
-                onChange={() => setShippingMode("delivery")}
-              />
-              <label htmlFor="delivery"> Delivery: 9AZN</label>
-            </div>
-          </div>
-          <div className="w-[34%] border border-1 p-2 border-l border-l-rose-500">
-            <div className="flex justify-between">
-              <p>Subtotal TTC:</p>
-              <p>{reducer}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Shipping:</p>
-              <p>{shippingMode === "free" ? "Free" : "9AZN"}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Total:</p>
-              <p>{shippingMode === "free" ? reducer : reducerDelivery}</p>
+              <div className="w-[34%] border border-1 p-2 border-l border-l-rose-500">
+                <div className="flex justify-between">
+                  <p>Subtotal TTC:</p>
+                  <p>{reducer}</p>
+                </div>
+                <div className="flex justify-between">
+                  <p>Shipping:</p>
+                  <p>{shippingMode === "free" ? "Free" : "9AZN"}</p>
+                </div>
+                <div className="flex justify-between">
+                  <p>Total:</p>
+                  <p>{shippingMode === "free" ? reducer : reducerDelivery}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className=" bg-gray-300 flex items-center justify-center w-full h-dvh">
+          <h1 className="text-[150px]">Empty cart</h1>
+        </div>
+      )}
     </>
   );
 };
--- src/pages/CheckOutMobil.js
+++ src/pages/CheckOutMobil.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment, deleteFunc } from "../features/cardSlice";
+import {
+  decrement,
+  increment,
+  deleteFunc,
+  addtoCart,
+} from "../features/cardSlice";
 
 const CheckOutMobil = () => {
   const dispatch = useDispatch();
@@ -36,94 +41,100 @@ const CheckOutMobil = () => {
   );
   return (
     <>
-      <div className="w-5/6 m-auto my-6 ">
-        {cartItems.map((item) => (
-          <div className="flex flex-col  justify-around my-4  border borer-1 ">
-            <div className="w-full h-48  m-auto">
-              <img
-                src={item.thumbnail}
-                className="w-full h-full m-auto object-container"
-                alt="sd"
-              />
+      {cartItems.length > 0 ? (
+        <div className="w-5/6 m-auto my-6 ">
+          {cartItems.map((item) => (
+            <div className="flex flex-col  justify-around my-4  border borer-1 ">
+              <div className="w-full h-48  m-auto">
+                <img
+                  src={item.thumbnail}
+                  className="w-full h-full m-auto object-container"
+                  alt="sd"
+                />
+              </div>
+              <div className="flex justify-between p-2">
+                <div>Title:</div>
+                <div>{item.title}</div>
+              </div>
+              <div className="flex justify-between p-2">
+                <div>Price:</div>
+                <div>{item.price}</div>
+              </div>
+              <div className="flex justify-between p-2">
+                <div>Quantity:</div>
+                <div>{item.quantity}</div>
+              </div>
+              <div className="flex justify-between p-2">
+                <div>Total:</div>
+                <div>{item.price * item.count}</div>
+              </div>
+              <div className="flex justify-between p-2">
+                <div className="">Count</div>
+                <div className="flex items-center justify-center gap-2 h-4 w-24">
+                  <button
+                    onClick={() => incrementBtn(item)}
+                    className="w-1/2 bg-gray-200 hover:bg-gray-400 "
+                  >
+                    -
+                  </button>
+                  <button className="w-1/2 bg-gray-600 "> {item.count}</button>
+                  <button
+                    onClick={() => decrementBtn(item)}
+                    className="w-1/2 bg-gray-200 hover:bg-gray-400 "
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="flex justify-between p-2">
-              <div>Title:</div>
-              <div>{item.title}</div>
+          ))}
+          <div className="flex max-md:flex-col max-md:w-full">
+            <div className="w-[100%] border border-1 p-2 border-r-0">
+              <h1>Choose shoppig mode:</h1>
+              <div>
+                <input
+                  type="radio"
+                  id="free"
+                  name="shippingMode"
+                  value="free"
+                  checked={shippingMode === "free"}
+                  onChange={() => setShippingMode("free")}
+                />
+                <label htmlFor="free"> Free</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="delivery"
+                  name="shippingMode"
+                  value="delivery"
+                  checked={shippingMode === "delivery"}
+                  onChange={() => setShippingMode("delivery")}
+                />
+                <label htmlFor="delivery"> Delivery: 9AZN</label>
+              </div>
             </div>
-            <div className="flex justify-between p-2">
-              <div>Price:</div>
-              <div>{item.price}</div>
-            </div>
-            <div className="flex justify-between p-2">
-              <div>Quantity:</div>
-              <div>{item.quantity}</div>
-            </div>
-            <div className="flex justify-between p-2">
-              <div>Total:</div>
-              <div>{item.price * item.count}</div>
-            </div>
-            <div className="flex justify-between p-2">
-              <div className="">Count</div>
-              <div className="flex items-center justify-center gap-2 h-4 w-24">
-                <button
-                  onClick={() => incrementBtn(item)}
-                  className="w-1/2 bg-gray-200 hover:bg-gray-400 "
-                >
-                  -
-                </button>
-                <button className="w-1/2 bg-gray-600 "> {item.count}</button>
-                <button
-                  onClick={() => decrementBtn(item)}
-                  className="w-1/2 bg-gray-200 hover:bg-gray-400 "
-                >
-                  +
-                </button>
+            <div className="w-[100%] border border-1 p-2 border-t-0">
+              <div className="flex justify-between">
+                <p>Subtotal TCC:</p>
+                <p>{reducer}</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Shipping:</p>
+                <p>{shippingMode === "free" ? "Free" : "9AZN"}</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Total:</p>
+                <p>{shippingMode === "free" ? reducer : reducerDelivery}</p>
               </div>
             </div>
           </div>
-        ))}
-        <div className="flex max-md:flex-col max-md:w-full">
-          <div className="w-[100%] border border-1 p-2 border-r-0">
-            <h1>Choose shoppig mode:</h1>
-            <div>
-              <input
-                type="radio"
-                id="free"
-                name="shippingMode"
-                value="free"
-                checked={shippingMode === "free"}
-                onChange={() => setShippingMode("free")}
-              />
-              <label htmlFor="free"> Free</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="delivery"
-                name="shippingMode"
-                value="delivery"
-                checked={shippingMode === "delivery"}
-                onChange={() => setShippingMode("delivery")}
-              />
-              <label htmlFor="delivery"> Delivery: 9AZN</label>
-            </div>
-          </div>
-          <div className="w-[100%] border border-1 p-2 border-t-0">
-            <div className="flex justify-between">
-              <p>Subtotal TCC:</p>
-              <p>{reducer}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Shipping:</p>
-              <p>{shippingMode === "free" ? "Free" : "9AZN"}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Total:</p>
-              <p>{shippingMode === "free" ? reducer : reducerDelivery}</p>
-            </div>
-          </div>
         </div>
-      </div>
+      ) : (
+        <div className=" mt-[-17px] bg-gray-300 flex items-center justify-center w-full h-dvh">
+          <h1 className="text-[100px] max-sm:text-[70px]">Empty cart</h1>
+        </div>
+      )}
     </>
   );
 };
